d8404692987eab75a0f728a3f897e4f5daf7526e
Administrator <Administrator@TEDU-20171026QE>
2018-06-05 17:41:20 +0800

    init scala

---
+++ src/Day01.scala
@@ -0,0 +1,17 @@
+object Day01 {
+  
+  def main(args: Array[String]): Unit = {
+     println(888) 
+     
+     val str:Double=123;
+     var str1=123;
+     
+     print(1+2+3)
+     print(1.+(2).+(3))
+     
+     print(new xbb().add(1, 2))
+     
+     
+  }
+  
+}
---
+++ src/xbb.scala
@@ -0,0 +1,10 @@
+
+
+class xbb{
+ 
+  def add(x1:Int,x2:Int) : Int ={
+    
+    return x1.+(x2)
+  }
+  
+}
